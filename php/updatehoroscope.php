aa52cb3e30a6fd0d8e313dfea3bb33a2bd1cb707
Jesper Hansson <32198001+yepsson@users.noreply.github.com>
2018-03-19 22:18:09 +0100

    Add files via upload

--- php/updatehoroscope.php
+++ php/updatehoroscope.php
@@ -1,37 +1,26 @@
 <?php
 parse_str(file_get_contents("php://input"), $_PUT);
-
 session_start();
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    
-    if (isset($_SESSION["mittHoroscop"])){
-        $key = "invalid";
-
-        if($horoscope->isValid()){
+if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
+    if(!isset($_SESSION["mittHoroscop"])){
+        $key = false;
+        echo "$key";
+    }
+    if(isset($_SESSION["mittHoroscop"])){
+        $key = 'invalid';
+        
         $_POST["socialnumber"] = $_PUT["socialnumber"];
         include "class.php";
-        $_SESSION["mittHoroscop"] = $horoscope->printSign();
         
-        $key = true;
+        if($horoscope->isValid()){
+            $_SESSION["mittHoroscop"] = $horoscope->printSign();
+            $key = true;
         }
         
-        
         echo "$key";
     }
-    elseif (!isset($_SESSION["mittHoroscop"])){
-
-    
-        $key= false;
-        echo "$key";
-    }
-    
 }
-
 else{
     echo "error";
 }
-
-
 ?>
